923b11d0765f46e47b17a85e21620ba74c106b62
Vaishnavi Katragadda <vaishnavi.katragadda.24@dartmouth.edu>
2021-01-27 22:29:11 -0500

    add initial project files.

---
+++ geom_analysis3.py
@@ -0,0 +1,32 @@
+import numpy
+import os
+
+def calculate_distance(atom1_coord, atom2_coord):
+    x_distance = atom1_coord[0] - atom2_coord[0]
+    y_distance = atom1_coord[1] - atom2_coord[1]
+    z_distance = atom1_coord[2] - atom2_coord[2]
+    bond_length_12 = numpy.sqrt(x_distance**2+y_distance**2+z_distance**2)
+    return bond_length_12
+
+def bond_check(atom_distance, minimum_length=0, maximum_length=1.5):
+    if atom_distance > minimum_length and atom_distance <= maximum_length:
+        return True
+    else:
+        return False
+
+def open_xyz(filename):
+     xyz_file = numpy.genfromtxt(fname=filename, skip_header=2, dtype='unicode')
+     symbols = xyz_file[:,0]
+     coord = (xyz_file[:,1:])
+     coord = coord.astype(numpy.float)
+     return symbols, coord
+
+file_location = os.path.join('data', 'water.xyz')
+symbols, coord = open_xyz(file_location)
+num_atoms = len(symbols)
+for num1 in range(0,num_atoms):
+     for num2 in range(0,num_atoms):
+         if num1<num2:
+             bond_length_12 = calculate_distance(coord[num1], coord[num2])
+             if bond_check(bond_length_12) is True:
+                 print(F'{symbols[num1]} to {symbols[num2]} : {bond_length_12:.3f}')
