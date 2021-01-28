4ea74fd900242dbbdc0004a808a97ce1757bdc30
Vaishnavi Katragadda <vaishnavi.katragadda.24@dartmouth.edu>
2021-01-27 22:31:07 -0500

    add docstring to geom_analysis.py

--- geom_analysis.py
+++ geom_analysis.py
@@ -1,3 +1,13 @@
+"""
+This module has functions associated with analyzing the geometry of a molecule.
+
+When run as a script and given an xyz file, this script will print out the bonds. Run
+
+$ python geometry_analysis.py --help
+
+to see input options.
+"""
+
 import numpy
 import os
 
